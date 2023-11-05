fc49888b53c6a26ef1838dab3ade9277c191798e
Jacob Christ <jacob@pontech.com>
2023-11-05 09:23:36 -0800

    Added Flood Prevention Timeout feature. Hopefully aligned the LED's with th SSRs for the REV-X1 PCB. Added LWT (Last Will and Testament) functionality. Added Manufacturer and Model names to MQTT reporting. Changed the data reporting prefix. Changed some uniqueId's for some entities.

--- src/HAIntegration.cpp
+++ src/HAIntegration.cpp
@@ -34,9 +34,9 @@ class ZoneData {
 };
 
 WiFiClient client;
-HADevice device;
+HADevice device("FlowBot-28cdc10aea3a"); // Hardcoding a MAC address here (until we figure out how to get it before or durring instantation)
 HAMqtt mqtt(client, device);
-HASwitch led("led", (void*) new ZoneData( LED_PIN, LED_PIN)); // unique identifier must not contain spaces
+HASwitch led("LED", (void*) new ZoneData( LED_PIN, LED_PIN)); // unique identifier must not contain spaces
 HASwitch zone[] = { 
     HASwitch("Zone0", (void*) new ZoneData( 6, 16)),
     HASwitch("Zone1", (void*) new ZoneData( 7, 17)),
@@ -50,8 +50,10 @@ HASwitch zone[] = {
     HASwitch("Zone9", (void*) new ZoneData(15, 28)),
 };
 
-uint8_t zone_pins[] = {  6,  7,  8,  9, 10, 11, 12, 13, 14, 15 };
+uint8_t zone_pins[] = { 11, 12, 13, 10, 14,  9, 15,  8,  7,  6 };
 uint8_t zled_pins[] = { 16, 17, 18, 19, 20, 21, 22, 26, 27, 28 };
+
+HANumber floodPreventionTimeoutSeconds("FPT", (HABaseDeviceType::NumberPrecision)0); //PrecisionP0);
 
 void HAIntegration::configure() {
 
@@ -85,11 +87,16 @@ void HAIntegration::configure() {
     byte mac[WL_MAC_ADDR_LENGTH];
     WiFi.macAddress(mac);
     device.setUniqueId(mac, sizeof(mac));
+    device.enableSharedAvailability(); // [OPTIONAL] Enables the ability to set the on/off line status of the device
+    device.setAvailability(true); // Set the device to on-line
+    device.enableLastWill(); // [OPTIONAL] Allows HA to detect when the device losses power (and thus disable the controls in the HA UI)
 
     //Device metadata:
 
-    device.setName("FlowBot by PONTECH");
+    device.setName("FlowBot");
     device.setSoftwareVersion("0.1");
+    device.setManufacturer("PONTECH.com");
+    device.setModel("FlowBot100");
 
     // handle switch state
     led.onCommand(switchHandler);
@@ -109,8 +116,18 @@ void HAIntegration::configure() {
         zone[i].setIcon("mdi:sprinkler-variant");
     }
 
+    floodPreventionTimeoutSeconds.onCommand(numberHandler);
+    floodPreventionTimeoutSeconds.setName("Flood Prevention Timeout"); // [OPTIONAL] Defualts to "MQTT Number"
+    floodPreventionTimeoutSeconds.setUnitOfMeasurement("seconds");
+    floodPreventionTimeoutSeconds.setMin(1);
+    floodPreventionTimeoutSeconds.setMax(65535);
+    //floodPreventionTimeoutSeconds.setCurrentState(HANumeric((uint32_t)600, (HABaseDeviceType::NumberPrecision)0)); // This set the starting value reported to HA
+    floodPreventionTimeoutSeconds.setCurrentState((uint32_t)(5*60)); // This set the starting value reported to HA
+
     Serial.print("Connecting to MQTT\n");
-    
+    //mqtt.setDiscoveryPrefix("homeassistant"); // [OPTIONAL] Default values is "homeassistant" and it must be this for automatic discovery
+    mqtt.setDataPrefix("plp"); // [OPTIONAL] This defaults to "aha" but you can change it without issue
+
     if (mqtt.begin(MQTT_BROKER, MQTT_LOGIN, MQTT_PASSWORD) == true) {
         Serial.print("Connected to MQTT broker\n");
     } else {
@@ -131,6 +148,10 @@ void HAIntegration::switchHandler(bool state, HASwitch* sender) {
         );
 }
 
+void HAIntegration::numberHandler(HANumeric number, HANumber* sender) {
+    sender->setState(number.toUInt32());  // report state back to Home Assistant
+    Serial.printf("%s new value: %d\n", sender->getName(), number.toUInt32());
+}
 
 void HAIntegration::loop() {
     mqtt.loop();
--- src/HAIntegration.h
+++ src/HAIntegration.h
@@ -8,6 +8,7 @@ class HAIntegration {
     void loop();
     void configure();
     static void switchHandler(bool state, HASwitch* sender);
+    static void numberHandler(HANumeric number, HANumber* sender);
 };
 
 #endif // SRC_HAINTEGRATION
